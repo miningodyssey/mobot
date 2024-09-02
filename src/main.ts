8ea04e88b3a16a5c0cc2a1fe196ed7a908605177
dunnogg <gamemode61@gmail.com>
2024-09-02 16:00:26 +0300

    fix bot start

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {BotService} from "./bot/bot.service";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const bot = app.get(BotService)
+  try {
+    bot.startBot()
+  } catch (err) {
+    console.log(err)
+    bot.startBot()
+  }
   await app.listen(3000);
 }
 bootstrap();
